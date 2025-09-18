c7e3a5406e34b2e24278108e29fb67244ef542e5
gottumukkalasowmya-prog <gottumukkala.sowmya@cognizant.com>
2025-09-18 17:27:37 +0530

    Update FreeListingPage.js

--- cypress/support/pageObjects/FreeListingPage.js
+++ cypress/support/pageObjects/FreeListingPage.js
@@ -2,28 +2,23 @@ class FreeListingPage {
   navigateToForm() {
     cy.get('#header_freelisting').click()
   }
- 
   fillFormWithInvalidPhone(phone) {
     cy.get('input[name="pincode"]').eq(0).type(phone);
    
   }
- 
   submitForm() {
     cy.contains("Start Now").click();
   }
- 
   captureErrorMessage() {
     cy.get('.entermobilenumber_error__text__uPM09').should('be.visible').then($text=>{
       cy.log($text.text());
     })
   }
- 
   triggerAlert() {
     cy.window().then((win) => {
       win.alert('Invalid input!');
     });
   }
- 
   captureAlertMessage() {
     cy.on('window:alert', (str) => {
       expect(str).to.equal('Invalid input!');
